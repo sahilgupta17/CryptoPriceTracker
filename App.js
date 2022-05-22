4bae8d3603f7cbfbfd6f50c87be5090f30af83ea
Sahil Gupta <sahgupta@umass.edu>
2022-05-21 20:42:18 -0700

    Initialised a file for storing colors to be used in the app

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "white" }}>
+      {/* <Text style={{ color: "white" }}>
         Open up App.js to start working on your app!
-      </Text>
+      </Text> */}
+      <View style={styles.coinContainer}></View>
       <StatusBar style="light" />
     </View>
   );
