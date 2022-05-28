62c9432320908dda250c582a703ad286921533e0
Sahil Gupta <sahgupta@umass.edu>
2022-05-27 19:47:24 -0700

    Initialised a screen which shows the details of a coin.

---
+++ src/screens/CoinDetailsScreen/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+const CoinDetailsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>CoinDetailsScreen</Text>
+    </View>
+  );
+};
+export default CoinDetailsScreen;
