7bb2e72129003a5e88f58f53d1392bdc2bdb115c
Sahil Gupta <sahgupta@umass.edu>
2022-06-01 01:45:28 -0700

    Refactored code into smaller components

--- src/screens/CoinDetailsScreen/index.jsx
+++ src/screens/CoinDetailsScreen/index.jsx
@@ -1,42 +1,17 @@
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import Coin from "../../../assets/data/crypto.json";
+import CoinDetailNavigation from "./components/CoinDetailNavigation";
 import CoinDetailHeader from "./components/CoinDetailHeader";
-import { AntDesign } from "@expo/vector-icons";
 import Graph from "./components/Graph";
-
 import styles from "./styles";
 import colors from "../../config/colors";
 
 const CoinDetailsScreen = () => {
-  const [priceData, setpriceData] = useState(
-    `$ ${Coin.market_data.price_change_24h.toFixed(
-      2
-    )} (${Coin.market_data.price_change_percentage_24h.toFixed(2)}%)`
-  );
-
-  const caretName =
-    Coin.market_data.price_change_24h >= 0 ? "caretup" : "caretdown";
-
-  const priceButtonColor =
-    Coin.market_data.price_change_24h >= 0
-      ? colors.profitGreen
-      : colors.lossRed;
   return (
     <View style={styles.container}>
-      <CoinDetailHeader image={Coin.image.small} symbol={Coin.symbol} />
-      <View style={styles.headerContainer}>
-        <Text style={styles.name}>{Coin.name}</Text>
-        <Text style={styles.currentPrice}>
-          $ {Coin.market_data.current_price.usd}
-        </Text>
-        <View style={styles.priceChangeDisplay}>
-          <AntDesign name={caretName} size={14} color={priceButtonColor} />
-          <Text style={[styles.priceChange, { color: priceButtonColor }]}>
-            {priceData}
-          </Text>
-        </View>
-      </View>
+      <CoinDetailNavigation image={Coin.image.small} symbol={Coin.symbol} />
+      <CoinDetailHeader />
     </View>
   );
 };
--- src/screens/CoinDetailsScreen/styles.js
+++ src/screens/CoinDetailsScreen/styles.js
@@ -3,45 +3,6 @@ import colors from "../../config/colors";
 
 const styles = StyleSheet.create({
   container: {},
-  navigation: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  name: {
-    fontSize: 18,
-    color: "white",
-  },
-  currentPrice: {
-    fontSize: 28,
-    color: "white",
-  },
-  headerContainer: {
-    // flexDirection: "row",
-    // alignItems: "center",
-    // justifyContent: "space-between",
-    padding: 20,
-  },
-  percentageChange: {
-    color: "white",
-    fontSize: 18,
-  },
-  priceChange: {
-    marginLeft: 5,
-    fontSize: 13,
-  },
-  percentagePriceChangeButton: {
-    backgroundColor: colors.profitGreen,
-    borderRadius: 5,
-    borderWidth: 1,
-    padding: 10,
-    width: 100,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  priceChangeDisplay: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
 });
 
 export default styles;
