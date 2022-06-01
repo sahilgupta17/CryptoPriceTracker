be879baf0d2573f1641a9932d1426bdafc0b5c43
Sahil Gupta <sahgupta@umass.edu>
2022-06-01 00:37:59 -0700

    Added header and button to display change in price in value as well as percentage

--- src/screens/CoinDetailsScreen/index.jsx
+++ src/screens/CoinDetailsScreen/index.jsx
@@ -1,16 +1,45 @@
-import React from "react";
-import { Image, View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
-import { Ionicons } from "@expo/vector-icons";
 import Coin from "../../../assets/data/crypto.json";
 import CoinDetailHeader from "../../components/CoinDetailHeader";
+import colors from "../../config/colors";
+
 const CoinDetailsScreen = () => {
+  const [priceData, setpriceData] = useState(
+    `$${Coin.market_data.price_change_percentage_24h.toFixed(2)}`
+  );
+  const onPercentagePriceChangeButtonPress = () => {
+    let priceChange = `$${Coin.market_data.price_change_24h.toFixed(2)}`;
+    let percentagePriceChange = `%${Coin.market_data.price_change_percentage_24h.toFixed(
+      2
+    )}`;
+    setpriceData(() =>
+      priceData == priceChange ? percentagePriceChange : priceChange
+    );
+  };
+  const priceButtonColor =
+    Coin.market_data.price_change_24h > 0 ? colors.profitGreen : colors.lossRed;
+
   return (
     <View style={styles.container}>
       <CoinDetailHeader image={Coin.image.small} symbol={Coin.symbol} />
-      <View style={styles.heading}>
-        <Text style={styles.title}>{Coin.name}</Text>
-        <Text style={styles.title}>{Coin.market_data.current_price.usd}</Text>
+      <View style={styles.headerContainer}>
+        <View>
+          <Text style={styles.name}>{Coin.name}</Text>
+          <Text style={styles.currentPrice}>
+            $ {Coin.market_data.current_price.usd}
+          </Text>
+        </View>
+        <TouchableOpacity
+          onPress={onPercentagePriceChangeButtonPress}
+          style={[
+            styles.percentagePriceChangeButton,
+            { backgroundColor: priceButtonColor },
+          ]}
+        >
+          <Text style={styles.percentagePriceChange}>{priceData}</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- src/screens/CoinDetailsScreen/styles.js
+++ src/screens/CoinDetailsScreen/styles.js
@@ -1,16 +1,38 @@
 import { StyleSheet } from "react-native";
+import colors from "../../config/colors";
 
 const styles = StyleSheet.create({
-  container: {
-    color: "white",
-  },
+  container: {},
   navigation: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  title: {
-    fontSize: 24,
+  name: {
+    fontSize: 18,
     color: "white",
+  },
+  currentPrice: {
+    fontSize: 28,
+    color: "white",
+  },
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  percentageChange: {
+    color: "white",
+    fontSize: 18,
+  },
+  percentagePriceChangeButton: {
+    backgroundColor: colors.profitGreen,
+    borderRadius: 5,
+    borderWidth: 1,
+    padding: 10,
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
