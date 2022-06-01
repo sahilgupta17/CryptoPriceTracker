16409675403e02d22d1f75f004c112ffa7146801
Sahil Gupta <sahgupta@umass.edu>
2022-06-01 01:43:51 -0700

    Initialised the Graph component

--- src/screens/CoinDetailsScreen/components/Graph/index.jsx
+++ src/screens/CoinDetailsScreen/components/Graph/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./styles";
 const Graph = () => {
-  <View style={styles.container}></View>;
+  return <View style={styles.container}></View>;
 };
 export default Graph;
