c97c669c730d611316571dc5196b89d34b0d2e20
Sahil Gupta <sahgupta@umass.edu>
2022-06-02 18:30:27 -0700

    Initialised Graph with color changing based on 24 hour difference

--- src/screens/CoinDetailsScreen/components/Graph/index.jsx
+++ src/screens/CoinDetailsScreen/components/Graph/index.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import { Dimensions, View } from "react-native";
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+} from "@rainbow-me/animated-charts";
+import Coin from "../../../../../assets/data/crypto.json";
 import styles from "./styles";
-const Graph = () => {
-  return <View style={styles.container}></View>;
+
+const Graph = (props) => {
+  const screenWidth = Dimensions.get("window").width;
+  let data = props.data;
+
+  return (
+    <View style={styles.container}>
+      <ChartPathProvider
+        data={{ data, smoothingStrategy: props.smoothingStrategy }}
+      >
+        <ChartPath
+          height={screenWidth / 2}
+          stroke={props.strokeColor}
+          width={screenWidth}
+        />
+        <ChartDot style={{ backgroundColor: props.chartDotColor }} />
+      </ChartPathProvider>
+    </View>
+  );
 };
 export default Graph;
