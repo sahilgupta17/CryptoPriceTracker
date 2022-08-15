4999f7617cc4adc74baf1048a70ed6f48ab1e04e
Sahil Gupta <sahgupta@umass.edu>
2022-08-15 14:52:43 -0700

    Removed Redundant code and added comments

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -2,6 +2,7 @@ import { Text, View, FlatList } from "react-native";
 import { CoinItem } from "../../components";
 import styles from "./styles";
 import cryptoData from "../../assets/data/cryptocurrencies.json";
+
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
@@ -10,8 +11,6 @@ const HomeScreen = () => {
         data={cryptoData}
         renderItem={({ item }) => <CoinItem coin={item} />}
       />
-
-      {/* <CoinItem /> */}
     </View>
   );
 };
