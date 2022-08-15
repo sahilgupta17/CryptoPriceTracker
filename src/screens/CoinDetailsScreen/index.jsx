da4e50fee8ef70ed0e6642a1b64dee8bc2be9016
Sahil Gupta <sahgupta@umass.edu>
2022-08-15 14:55:43 -0700

    Initialised Coin Details Screen

---
+++ src/screens/CoinDetailsScreen/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const CoinDetailsScreen = () => {
+  return (
+    <View>
+      <Text>Coin Details Screen</Text>
+    </View>
+  );
+};
+export default CoinDetailsScreen;
---
+++ src/screens/CoinDetailsScreen/styles.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-native";
+import theme from "../../config/themes";
+
+const styles = StyleSheet.create({});
+export default styles;
